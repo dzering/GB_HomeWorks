45767aff3277d95653b9620ab63089638744c93d
dzering <71943051+dzering@users.noreply.github.com>
2022-12-05 11:01:33 +0300

    Task3 was complited

--- Assets/Scripts/Exercice2.cs
+++ Assets/Scripts/Exercice2.cs
@@ -15,23 +15,48 @@ public class Exercice2 : MonoBehaviour
         }
     }
 
-    private void Start()
+    private async void Start()
     {
-        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-        CancellationToken ct = cancellationTokenSource.Token;
+        using (CancellationTokenSource cancellationTokenSource = new CancellationTokenSource())
+        {
 
-        Task_1(ct);
-        Task_2(ct);
+            CancellationToken ct = cancellationTokenSource.Token;
+
+            Task task1 = Task_1(ct);
+            Task task2 = Task_2(ct);
+
+            bool res = await WhatTaskFasterAsync(ct, task1, task2);
+            cancellationTokenSource.Cancel();
+
+            Debug.Log(res);
+            
+        }
+
     }
 
+    private async Task<bool> WhatTaskFasterAsync(CancellationToken ct, Task task1, Task task2)
+    {
+        int task1Hash = task1.GetHashCode();
+        int task2Hash = task2.GetHashCode();
 
-    private async void Task_1(CancellationToken ct)
+        Debug.Log($"Task1Hash {task1Hash}\nTask2Hash {task2Hash}");
+
+        Task res = await Task.WhenAny(task1, task2);
+        Debug.Log(res.GetHashCode());
+
+        if (res.GetHashCode() == task1.GetHashCode())
+            return true;
+        else
+            return false;
+    }
+
+    private async Task Task_1(CancellationToken ct)
     {
         await Task.Delay(1000);
         Debug.Log("Delay 1s");
     }
 
-    private async void Task_2(CancellationToken ct)
+    private async Task Task_2(CancellationToken ct)
     {
         int count = 0;
         while(count < 60)
@@ -43,4 +68,6 @@ public class Exercice2 : MonoBehaviour
         }
         Debug.Log("Delay 60 frame");
     }
+
+
 }
